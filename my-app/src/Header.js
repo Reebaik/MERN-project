acf4ba90fb9325b9cbf13ea858152af987af0bab
Reebaik <Reebaikrox@gmail.com>
2024-07-26 18:45:03 +0530

    Initial commit

---
+++ my-app/src/Header.js
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom"
+import { useEffect, useState,useContext } from "react"
+import {UserContext} from "./Usercontext";
+
+export default function Header(){
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">Reebaiks Blog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create Newpost</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+    
+
+ 
